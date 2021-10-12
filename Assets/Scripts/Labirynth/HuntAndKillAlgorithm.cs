567af050ec5ab501b852ef96988d5388de7e07ed
Dawid Kaluża <dawid.kaluza@tensquaregames.com>
2021-10-12 21:09:32 +0200

    maze generator refactor part 2

--- Assets/Scripts/Labirynth/HuntAndKillAlgorithm.cs
+++ Assets/Scripts/Labirynth/HuntAndKillAlgorithm.cs
@@ -1,15 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Utils;
 
 namespace Labirynth
 {
     public class HuntAndKillAlgorithm : GenerationAlgorithm
-    {
-        private static readonly Vector2Int North = new Vector2Int(-1, 0);
-        private static readonly Vector2Int South = new Vector2Int(1, 0);
-        private static readonly Vector2Int East = new Vector2Int(0, 1);
-        private static readonly Vector2Int West = new Vector2Int(0, -1);
-
+    { 
         private int _currentRow;
         private int _currentColumn;
 
@@ -50,6 +46,32 @@ namespace Labirynth
             }
         }
 
+        private void KillNextCell()
+        {
+            var direction = ProceduralNumberGenerator.GetNextNumber();
+            switch (direction)
+            {
+                case 1 when CellAvailable(_currentRow - 1, _currentColumn):
+                    GoNorth(_currentRow, _currentColumn);
+                    _currentRow--;
+                    break;
+                case 2 when CellAvailable(_currentRow + 1, _currentColumn):
+                    GoSouth(_currentRow, _currentColumn);
+                    _currentRow++;
+                    break;
+                case 3 when CellAvailable(_currentRow, _currentColumn - 1):
+                    GoEast(_currentRow, _currentColumn);
+                    _currentColumn--;
+                    break;
+                case 4 when CellAvailable(_currentRow, _currentColumn + 1):
+                    GoWest(_currentRow, _currentColumn);
+                    _currentColumn++;
+                    break;
+            }
+
+            Cells[_currentRow, _currentColumn].Visited = true;
+        }
+
         private void Hunt()
         {
             _courseComplete = true;
@@ -77,33 +99,47 @@ namespace Labirynth
             return true;
         }
 
-        private void KillNextCell()
+        private void DestroyAdjacentWall(int row, int column)
         {
+            var availableDirections = GetAvailableDirections(row, column);
             var direction = ProceduralNumberGenerator.GetNextNumber();
-
-            // todo potential refactor but it works
-            switch (direction)
+            while (!availableDirections.Contains(direction))
             {
-                case 1 when CellAvailable(_currentRow - 1, _currentColumn):
-                    GoNorth(_currentRow, _currentColumn);
-                    _currentRow--;
-                    break;
-                case 2 when CellAvailable(_currentRow + 1, _currentColumn):
-                    GoSouth(_currentRow, _currentColumn);
-                    _currentRow++;
-                    break;
-                case 3 when CellAvailable(_currentRow, _currentColumn - 1):
-                    GoEast(_currentRow, _currentColumn);
-                    _currentColumn--;
-                    break;
-                case 4 when CellAvailable(_currentRow, _currentColumn + 1):
-                    GoWest(_currentRow, _currentColumn);
-                    _currentColumn++;
-                    break;
+                direction = ProceduralNumberGenerator.GetNextNumber();
             }
 
-            Cells[_currentRow, _currentColumn].Visited = true;
+            switch (direction)
+            {
+                case 1:
+                    GoNorth(row, column);
+                    break;
+                case 2:
+                    GoSouth(row, column);
+                    break;
+                case 3:
+                    GoEast(row, column);
+                    break;
+                case 4:
+                    GoWest(row, column);
+                    break;
+            }
         }
+
+        private void GenerateStartingArea(Vector2Int size)
+        {
+            for (var i = Dimensions.x / 2 - size.x; i < Dimensions.x / 2 + size.x; i++)
+            {
+                for (var j = Dimensions.y / 2 - size.y; j < Dimensions.y / 2 + size.y; j++)
+                {
+                    Cells[i, j].North.DestroyIfExist();
+                    Cells[i, j].South.DestroyIfExist();
+                    Cells[i, j].East.DestroyIfExist();
+                    Cells[i, j].West.DestroyIfExist();
+                }
+            }
+        }
+
+        #region Helper Functions
 
         private bool RouteAvailable(int row, int column, int offset = 1)
         {
@@ -127,86 +163,76 @@ namespace Labirynth
                    CanGoWest(row, column);
         }
 
-        private void DestroyAdjacentWall(int row, int column)
-        {
-            var wallDestroyed = false;
-
-            while (!wallDestroyed)
-            {
-                // int direction = Random.Range (1, 5);
-                var direction = ProceduralNumberGenerator.GetNextNumber();
-
-                switch (direction)
-                {
-                    case 1 when CanGoNorth(row, column):
-                        GoNorth(row, column);
-                        wallDestroyed = true;
-                        break;
-                    case 2 when CanGoSouth(row, column):
-                        GoSouth(row, column);
-                        wallDestroyed = true;
-                        break;
-                    case 3 when CanGoEast(row, column):
-                        GoEast(row, column);
-                        wallDestroyed = true;
-                        break;
-                    case 4 when CanGoWest(row, column):
-                        GoWest(row, column);
-                        wallDestroyed = true;
-                        break;
-                }
-            }
-        }
-        
         private bool CanGoNorth(int row, int column)
         {
             return row > 0 && Cells[row - 1, column] != null && Cells[row - 1, column].Visited;
         }
+
         private bool CanGoSouth(int row, int column)
         {
             return row < (Dimensions.x - 2) && Cells[row + 1, column] != null && Cells[row + 1, column].Visited;
         }
+
         private bool CanGoEast(int row, int column)
         {
             return column > 0 && Cells[row, column - 1] != null && Cells[row, column - 1].Visited;
         }
+
         private bool CanGoWest(int row, int column)
         {
             return column < (Dimensions.y - 2) && Cells[row, column + 1] != null && Cells[row, column + 1].Visited;
         }
+
         private void GoNorth(int row, int column)
         {
             Cells[row, column].North.DestroyIfExist();
             Cells[row - 1, column].South.DestroyIfExist();
         }
+
         private void GoSouth(int row, int column)
         {
             Cells[row, column].South.DestroyIfExist();
             Cells[row + 1, column].North.DestroyIfExist();
         }
+
         private void GoEast(int row, int column)
         {
             Cells[row, column].West.DestroyIfExist();
             Cells[row, column - 1].East.DestroyIfExist();
         }
+
         private void GoWest(int row, int column)
         {
             Cells[row, column].East.DestroyIfExist();
             Cells[row, column + 1].West.DestroyIfExist();
         }
 
-        private void GenerateStartingArea(Vector2Int size)
+        private List<int> GetAvailableDirections(int row, int column)
         {
-            for (var i = Dimensions.x / 2 - size.x; i < Dimensions.x / 2 + size.x; i++)
+            var availableDirections = new List<int>();
+            if (CanGoNorth(row, column))
             {
-                for (var j = Dimensions.y / 2 - size.y; j < Dimensions.y / 2 + size.y; j++)
-                {
-                    Cells[i, j].North.DestroyIfExist();
-                    Cells[i, j].South.DestroyIfExist();
-                    Cells[i, j].East.DestroyIfExist();
-                    Cells[i, j].West.DestroyIfExist();
-                }
+                availableDirections.Add(1);
             }
+
+            if (CanGoSouth(row, column))
+            {
+                availableDirections.Add(2);
+            }
+
+            if (CanGoEast(row, column))
+            {
+                availableDirections.Add(3);
+            }
+
+            if (CanGoWest(row, column))
+            {
+                availableDirections.Add(4);
+            }
+
+            return availableDirections;
         }
+
+        #endregion
     }
 }
