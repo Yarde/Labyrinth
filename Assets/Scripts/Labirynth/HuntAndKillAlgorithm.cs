623792482ac8400a9873336b8121748d94031663
Dawid Kaluża <dawid.kaluza@tensquaregames.com>
2021-10-12 20:52:55 +0200

    maze generator refactor

--- Assets/Scripts/Labirynth/HuntAndKillAlgorithm.cs
+++ Assets/Scripts/Labirynth/HuntAndKillAlgorithm.cs
@@ -5,6 +5,11 @@ namespace Labirynth
 {
     public class HuntAndKillAlgorithm : GenerationAlgorithm
     {
+        private static readonly Vector2Int North = new Vector2Int(-1, 0);
+        private static readonly Vector2Int South = new Vector2Int(1, 0);
+        private static readonly Vector2Int East = new Vector2Int(0, 1);
+        private static readonly Vector2Int West = new Vector2Int(0, -1);
+
         private int _currentRow;
         private int _currentColumn;
 
@@ -17,6 +22,10 @@ namespace Labirynth
         public override void CreateMaze()
         {
             HuntAndKill();
+
+            // Clears the start area
+            var size = new Vector2Int(Dimensions.x / 10, Dimensions.y / 10);
+            GenerateStartingArea(size);
         }
 
         private void HuntAndKill()
@@ -31,62 +40,13 @@ namespace Labirynth
                 Kill(); // Will run until it hits a dead end.
                 Hunt(); // Finds the next unvisited cell with an adjacent visited cell. If it can't find any, it sets courseComplete to true.
             }
-
-            // Clears the start area
-            var size = new Vector2Int(Dimensions.x/10, Dimensions.y/10);
-            GenerateStartingArea(size);
-        }
-
-        private void GenerateStartingArea(Vector2Int size)
-        {
-            for (var i = Dimensions.x / 2 - size.x; i < Dimensions.x / 2 + size.x; i++)
-            {
-                for (var j = Dimensions.y / 2 - size.y; j < Dimensions.y / 2 + size.y; j++)
-                {
-                    DestroyWallIfItExists(Cells[i, j].North);
-                    DestroyWallIfItExists(Cells[i, j].South);
-                    DestroyWallIfItExists(Cells[i, j].East);
-                    DestroyWallIfItExists(Cells[i, j].West);
-                }
-            }
         }
 
         private void Kill()
         {
-            while (RouteStillAvailable(_currentRow, _currentColumn))
+            while (RouteAvailable(_currentRow, _currentColumn))
             {
-                //var direction = Random.Range(1, 5);
-                var direction = ProceduralNumberGenerator.GetNextNumber();
-
-                switch (direction)
-                {
-                    case 1 when CellIsAvailable(_currentRow - 1, _currentColumn):
-                        // North
-                        DestroyWallIfItExists(Cells[_currentRow, _currentColumn].North);
-                        DestroyWallIfItExists(Cells[_currentRow - 1, _currentColumn].South);
-                        _currentRow--;
-                        break;
-                    case 2 when CellIsAvailable(_currentRow + 1, _currentColumn):
-                        // South
-                        DestroyWallIfItExists(Cells[_currentRow, _currentColumn].South);
-                        DestroyWallIfItExists(Cells[_currentRow + 1, _currentColumn].North);
-                        _currentRow++;
-                        break;
-                    case 3 when CellIsAvailable(_currentRow, _currentColumn + 1):
-                        // East
-                        DestroyWallIfItExists(Cells[_currentRow, _currentColumn].East);
-                        DestroyWallIfItExists(Cells[_currentRow, _currentColumn + 1].West);
-                        _currentColumn++;
-                        break;
-                    case 4 when CellIsAvailable(_currentRow, _currentColumn - 1):
-                        // West
-                        DestroyWallIfItExists(Cells[_currentRow, _currentColumn].West);
-                        DestroyWallIfItExists(Cells[_currentRow, _currentColumn - 1].East);
-                        _currentColumn--;
-                        break;
-                }
-
-                Cells[_currentRow, _currentColumn].Visited = true;
+                KillNextCell();
             }
         }
 
@@ -94,96 +54,77 @@ namespace Labirynth
         {
             _courseComplete = true;
 
-            for (var r = 0; r < Dimensions.x; r++)
+            for (var row = 0; row < Dimensions.x; row++)
             {
-                for (var c = 0; c < Dimensions.y; c++)
+                for (var column = 0; column < Dimensions.y; column++)
                 {
-                    if (Cells[r, c] == null) continue;
-
-                    if (Cells[r, c].Visited || !CellHasAnAdjacentVisitedCell(r, c)) continue;
-
-                    _courseComplete = false;
-                    _currentRow = r;
-                    _currentColumn = c;
-                    DestroyAdjacentWall(_currentRow, _currentColumn);
-                    Cells[_currentRow, _currentColumn].Visited = true;
-                    return;
+                    if (HuntCell(row, column))
+                        return;
                 }
             }
         }
 
-
-        private bool RouteStillAvailable(int row, int column)
+        private bool HuntCell(int row, int column)
         {
-            var availableRoutes = 0;
+            if (Cells[row, column] == null) return false;
+            if (Cells[row, column].Visited || !HasAdjacentVisitedCell(row, column)) return false;
 
-            if (row > 0 && Cells[row - 1, column] != null && !Cells[row - 1, column].Visited)
-            {
-                availableRoutes++;
-            }
-
-            if (row < Dimensions.x - 1 && Cells[row + 1, column] != null && !Cells[row + 1, column].Visited)
-            {
-                availableRoutes++;
-            }
-
-            if (column > 0 && Cells[row, column - 1] != null && !Cells[row, column - 1].Visited)
-            {
-                availableRoutes++;
-            }
-
-            if (column < Dimensions.y - 1 && Cells[row, column + 1] != null && !Cells[row, column + 1].Visited)
-            {
-                availableRoutes++;
-            }
-
-            return availableRoutes > 0;
+            _courseComplete = false;
+            _currentRow = row;
+            _currentColumn = column;
+            DestroyAdjacentWall(row, column);
+            Cells[row, column].Visited = true;
+            return true;
         }
 
-        private bool CellIsAvailable(int row, int column)
+        private void KillNextCell()
+        {
+            var direction = ProceduralNumberGenerator.GetNextNumber();
+
+            // todo potential refactor but it works
+            switch (direction)
+            {
+                case 1 when CellAvailable(_currentRow - 1, _currentColumn):
+                    GoNorth(_currentRow, _currentColumn);
+                    _currentRow--;
+                    break;
+                case 2 when CellAvailable(_currentRow + 1, _currentColumn):
+                    GoSouth(_currentRow, _currentColumn);
+                    _currentRow++;
+                    break;
+                case 3 when CellAvailable(_currentRow, _currentColumn - 1):
+                    GoEast(_currentRow, _currentColumn);
+                    _currentColumn--;
+                    break;
+                case 4 when CellAvailable(_currentRow, _currentColumn + 1):
+                    GoWest(_currentRow, _currentColumn);
+                    _currentColumn++;
+                    break;
+            }
+
+            Cells[_currentRow, _currentColumn].Visited = true;
+        }
+
+        private bool RouteAvailable(int row, int column, int offset = 1)
+        {
+            return row > 0 && Cells[row - 1, column] != null && !Cells[row - 1, column].Visited ||
+                   row < Dimensions.x - offset && Cells[row + 1, column] != null && !Cells[row + 1, column].Visited ||
+                   column > 0 && Cells[row, column - 1] != null && !Cells[row, column - 1].Visited ||
+                   column < Dimensions.y - offset && Cells[row, column + 1] != null && !Cells[row, column + 1].Visited;
+        }
+
+        private bool CellAvailable(int row, int column)
         {
             return row >= 0 && row < Dimensions.x && column >= 0 && column < Dimensions.y &&
                    Cells[row, column] != null && !Cells[row, column].Visited;
         }
 
-        private void DestroyWallIfItExists(Object wall)
+        private bool HasAdjacentVisitedCell(int row, int column)
         {
-            if (wall != null)
-            {
-                Object.Destroy(wall);
-            }
-        }
-
-        private bool CellHasAnAdjacentVisitedCell(int row, int column)
-        {
-            var visitedCells = 0;
-
-            // Look 1 row up (north) if we're on row 1 or greater
-            if (row > 0 && Cells[row - 1, column] != null && Cells[row - 1, column].Visited)
-            {
-                visitedCells++;
-            }
-
-            // Look one row down (south) if we're the second-to-last row (or less)
-            if (row < (Dimensions.x - 2) && Cells[row + 1, column] != null && Cells[row + 1, column].Visited)
-            {
-                visitedCells++;
-            }
-
-            // Look one row left (west) if we're column 1 or greater
-            if (column > 0 && Cells[row, column - 1] != null && Cells[row, column - 1].Visited)
-            {
-                visitedCells++;
-            }
-
-            // Look one row right (east) if we're the second-to-last column (or less)
-            if (column < (Dimensions.y - 2) && Cells[row, column + 1] != null && Cells[row, column + 1].Visited)
-            {
-                visitedCells++;
-            }
-
-            // return true if there are any adjacent visited _cells to this one
-            return visitedCells > 0;
+            return CanGoNorth(row, column) ||
+                   CanGoSouth(row, column) ||
+                   CanGoEast(row, column) ||
+                   CanGoWest(row, column);
         }
 
         private void DestroyAdjacentWall(int row, int column)
@@ -197,28 +138,73 @@ namespace Labirynth
 
                 switch (direction)
                 {
-                    case 1 when row > 0 && Cells[row - 1, column] != null && Cells[row - 1, column].Visited:
-                        DestroyWallIfItExists(Cells[row, column].North);
-                        DestroyWallIfItExists(Cells[row - 1, column].South);
+                    case 1 when CanGoNorth(row, column):
+                        GoNorth(row, column);
                         wallDestroyed = true;
                         break;
-                    case 2 when row < (Dimensions.x - 2) && Cells[row + 1, column] != null &&
-                                Cells[row + 1, column].Visited:
-                        DestroyWallIfItExists(Cells[row, column].South);
-                        DestroyWallIfItExists(Cells[row + 1, column].North);
+                    case 2 when CanGoSouth(row, column):
+                        GoSouth(row, column);
                         wallDestroyed = true;
                         break;
-                    case 3 when column > 0 && Cells[row, column - 1] != null && Cells[row, column - 1].Visited:
-                        DestroyWallIfItExists(Cells[row, column].West);
-                        DestroyWallIfItExists(Cells[row, column - 1].East);
+                    case 3 when CanGoEast(row, column):
+                        GoEast(row, column);
                         wallDestroyed = true;
                         break;
-                    case 4 when column < (Dimensions.y - 2) && Cells[row, column + 1] != null &&
-                                Cells[row, column + 1].Visited:
-                        DestroyWallIfItExists(Cells[row, column].East);
-                        DestroyWallIfItExists(Cells[row, column + 1].West);
+                    case 4 when CanGoWest(row, column):
+                        GoWest(row, column);
                         wallDestroyed = true;
                         break;
+                }
+            }
+        }
+        
+        private bool CanGoNorth(int row, int column)
+        {
+            return row > 0 && Cells[row - 1, column] != null && Cells[row - 1, column].Visited;
+        }
+        private bool CanGoSouth(int row, int column)
+        {
+            return row < (Dimensions.x - 2) && Cells[row + 1, column] != null && Cells[row + 1, column].Visited;
+        }
+        private bool CanGoEast(int row, int column)
+        {
+            return column > 0 && Cells[row, column - 1] != null && Cells[row, column - 1].Visited;
+        }
+        private bool CanGoWest(int row, int column)
+        {
+            return column < (Dimensions.y - 2) && Cells[row, column + 1] != null && Cells[row, column + 1].Visited;
+        }
+        private void GoNorth(int row, int column)
+        {
+            Cells[row, column].North.DestroyIfExist();
+            Cells[row - 1, column].South.DestroyIfExist();
+        }
+        private void GoSouth(int row, int column)
+        {
+            Cells[row, column].South.DestroyIfExist();
+            Cells[row + 1, column].North.DestroyIfExist();
+        }
+        private void GoEast(int row, int column)
+        {
+            Cells[row, column].West.DestroyIfExist();
+            Cells[row, column - 1].East.DestroyIfExist();
+        }
+        private void GoWest(int row, int column)
+        {
+            Cells[row, column].East.DestroyIfExist();
+            Cells[row, column + 1].West.DestroyIfExist();
+        }
+
+        private void GenerateStartingArea(Vector2Int size)
+        {
+            for (var i = Dimensions.x / 2 - size.x; i < Dimensions.x / 2 + size.x; i++)
+            {
+                for (var j = Dimensions.y / 2 - size.y; j < Dimensions.y / 2 + size.y; j++)
+                {
+                    Cells[i, j].North.DestroyIfExist();
+                    Cells[i, j].South.DestroyIfExist();
+                    Cells[i, j].East.DestroyIfExist();
+                    Cells[i, j].West.DestroyIfExist();
                 }
             }
         }
---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class Utils
+    {
+        public static void DestroyIfExist(this GameObject gameObject)
+        {
+            if (gameObject != null)
+            {
+                Object.Destroy(gameObject);
+            }
+        }
+    }
+}
