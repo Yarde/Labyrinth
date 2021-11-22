e7a7ac751272cee4e15d53c502cabd789bf0a4de
Dawid Kaluża <dawid.kaluza@tensquaregames.com>
2021-11-22 12:17:56 +0100

    trigger fancy particles and animations

--- Assets/Scripts/Labirynth/Questions/Key.cs
+++ Assets/Scripts/Labirynth/Questions/Key.cs
@@ -1,13 +1,24 @@
 ﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace Labirynth.Questions
 {
     public class Key : QuestionTrigger
     {
-
+        [SerializeField] private Animator animator;
+        [SerializeField] private Collider collider;
+        private static readonly int DestroyTrigger = Animator.StringToHash("Destroy");
+        
         public override async UniTask Destroy()
         {
             Collected = true;
+            
+            animator.SetTrigger(DestroyTrigger);
+            
+            collider.enabled = false;
+            var animationLength = animator.GetCurrentAnimatorStateInfo(0).length;
+            await UniTask.Delay((int)(animationLength * 1000));
+            
             Destroy(gameObject);
             await UniTask.CompletedTask;
         }
